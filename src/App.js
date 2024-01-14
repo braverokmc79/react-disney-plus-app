b01e596ec28a0e6c0de6a14eaec02531055022c9
braverokmc79 <braverokmc79@gmail.com>
2024-01-14 22:47:43 +0900

    09.Styled Component를 이용한 비디오 배넌 생성 하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import styled from 'styled-components';
 import './App.css';
 import Nav from './components/Nav';
 
+const Container =styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  top:72px;  
+  padding:0 calc( 3.5vw + 5px);
+
+  &:after {
+    content:"";
+    background: url("/images/home-background.png") center center / cover no-repeat  fixed;
+    position: absolute;
+    inset: 0px;
+    opacity:1;
+    z-index: -1;
+  }
+
+`;
+
+
 function App() {
   return (
-    <div className="App">
+    <Container className="App">
       <Nav />
-    </div>
+    </Container>
   );
 }
 
 export default App;
+
+
+
