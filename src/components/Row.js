ea76d54c433854b1cead8e757133eef6254eddd1
braverokmc79 <braverokmc79@gmail.com>
2024-01-18 19:01:07 +0900

    18.swiper 모듈 슬라이드를 위한 css 작성하기

--- src/components/Row.js
+++ src/components/Row.js
@@ -131,32 +131,3 @@ const Container=styled.div`
   padding: 0, 0 30px;
 
 `;
-
-const Wrap=styled.div`
-    width:95%;
-    height:95%;
-    padding-top:56.25%;
-    boarder-radius:10px;
-    box-shadow: 
-      rgb(0 0 0/69%) 0px 26px 30px -10px,
-      rgb(0 0 0 0/73%) 0px 16px 10px -10px;
-    cursor:pointer;
-    overflow:hidden;
-    position:relative;
-    transition:all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
-    border:3px solid rgba(249, 249, 249, 0.1);
-
-    img{
-      inset:0;
-      display:block;
-      height:100%;
-      obj-fit:cover;
-      opacity:1;
-      position:absolute;
-      width:100%;
-      transition:opacity 500ms ease-in-out;
-      z-index:1;
-    }
-
-`;
-
