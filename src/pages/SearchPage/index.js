7c97fd87eca95687573e5b63aff0584b6c95f683
braverokmc79 <braverokmc79@gmail.com>
2024-01-18 15:09:05 +0900

    13.검색 페이지 UI 구현하기

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,7 +1,9 @@
 import _ from 'lodash';
 import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { axiosInnstance } from '../../api/axios';
+import './SearchPage.css';
+
 
 //Throttle
 let timer;
@@ -16,8 +18,12 @@ function debounce(callbackFn, timeout) {
 }
 
 
+
 const SearchPage = () => {
   const [searchResults, setSearchResults] = useState([]);
+  const navigate=useNavigate();
+
+
   const useQuery = () => {
     return new URLSearchParams(useLocation().search);
   };
@@ -28,29 +34,67 @@ const SearchPage = () => {
   useEffect(() => {
     if(searchTerm){
       debounce(() => {      
-        console.log('searchTerm ',searchTerm);   
-        fetchSearchMovie(searchTerm);   
+        fetchSearchMovie(searchTerm);            
+        console.log(searchResults.length);
       }, 300);     
     }
-
-
   }, [searchTerm]);
 
 
   const fetchSearchMovie = async(searchTerm) => {
       try {
         const response=await axiosInnstance.get(`/search/multi?include_adult=false&query=${searchTerm}`);
-        console.log('response ',response);          
+        setSearchResults(response.data.results);
+            
       } catch (error) {
         console.error('error ',error);
       }
   }
+  
+
+  if(searchResults.length > 0){
+    return (
+      <section className='search-container'>
+          {
+            searchResults.map((movie) => {
+             
+              if(movie.backdrop_path!=null && movie.media_type!=="person"){
+                  const moiveImageUrl=`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`;
+
+                    return  (
+                        <div className='movie' key={movie.id}>
+                          <div className='movie__column-poster' onClick={()=>navigate(`/${movie.id}`)}>
+                            <img src={moiveImageUrl} alt={movie.title}  className='movie__poster'     />
+                           </div>                           
+                        </div>
+                    );
+                      
+               }else{
+                    return null;
+               }
+            
+            })
+
+            
+          }
+      </section>
+    )
+
+  }else{
+
+      return (
+        <section className='no-results'>
+          <div className='no-results__text'>
+            <p>
+                찾고자하는 검색어 "{searchTerm}"  에 맞는 영화가 없습니다.
+            </p>
+          </div>
+        </section>
+      )
+  }
+  
 
 
-
-  return (
-    <div>SearchPage</div>
-  )
 }
 
 export default SearchPage
